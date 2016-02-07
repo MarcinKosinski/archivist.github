d5067cba3f8c4bffac7d6cd8071493fef08a6f09
MarcinKosinski <kosinskim@student.mini.pw.edu.pl>
2016-02-07 20:05:59 +0100

    Few basic files

---
+++ R/archivist.github-package.R
@@ -0,0 +1,35 @@
+##    archivist package for R
+##		archivist.github package for R
+##
+#' @title Tools for Archiving, Managing and Sharing R Objects via GitHub
+#'
+#' @description
+#' TODO
+#'
+#' @details
+#' For more detailed information visit \pkg{archivist} \code{wiki} on 
+#' \href{https://github.com/pbiecek/archivist/wiki}{Github}.
+#'
+#' @author
+#' Przemyslaw Biecek [aut, cre] \email{przemyslaw.biecek@@gmail.com} \cr
+#' Marcin Kosinski [aut] \email{m.p.kosinski@@gmail.com}
+#' 
+#' @import httr
+#' @import archivist
+#' @import git2r
+#' 
+#' @family archivist
+#' @name archivist-package
+#' @docType package
+invisible(NULL)
+#' Pipe operator
+#'
+#' See \code{\link[magrittr]{\%>\%}} for more details.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+NULL
