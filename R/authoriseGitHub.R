c5b6fc74867255c1c8f4bc0913d8cc7c2cec539a
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2016-03-26 23:03:00 +0100

    function for shorter authorisation with github API

---
+++ R/authoriseGitHub.R
@@ -0,0 +1,34 @@
+#' @title Authorise with GitHub API
+#'
+#' @description
+#' 
+#' \code{authoriseGitHub} is function that performes OAuth authorisation with GitHub API 
+#' and stores resulting token in the \code{github_token} variable.
+#' In order to authorise your app you need ClinetID and ClientSecret.
+#' They can be found here: https://github.com/settings/applications/new
+#' 
+#' @param ClientID A 20 characters long string with Client ID. See https://github.com/settings/applications/ for more details.
+#' 
+#' @param ClientSecret A 40 characters long string with Client Secret. See https://github.com/settings/applications/ for more details.
+#' 
+#' @author 
+#' Przemyslaw Biecek, \email{przemyslaw.biecek@@gmail.com}
+#'
+#' @examples
+#' \dontrun{
+#' ## GitHub version
+#' authoriseGitHub(ClientID, ClientSecret)
+#' }
+#' @family archivist.github
+#' @rdname authoriseGitHub
+#' @export
+authoriseGitHub <- function(ClientID,	ClientSecret) {
+	myapp <- oauth_app("github",
+	                    key = ClientID,
+	                    secret = ClientSecret)
+	github_token <- oauth2.0_token(oauth_endpoints("github"),
+	                                 myapp,
+	                                 scope = "public_repo")
+	aoptions("github_token", github_token)
+	github_token
+}
